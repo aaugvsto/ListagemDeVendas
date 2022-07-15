8a3ac70309282420fe9954021b4c62e0e88a0d32
Augusto Assis <augustoflas@outlook.com>
2022-07-15 20:34:00 -0300

    send notification

--- frontend/src/components/RedButtonUser/index.tsx
+++ frontend/src/components/RedButtonUser/index.tsx
@@ -1,9 +1,23 @@
+import axios from 'axios';
 import icon from '../../assets/img/IconUser.svg'
+import { BASE_URL } from '../../utils/request';
 import './style.css'
 
-function RedButtonUser(){
+type Props = {
+     saleId: number;
+
+}
+
+function handleClick(saleId: number){
+    axios(`${BASE_URL}/sales/${saleId}/notification`)
+        .then(res => {
+            console.log("Sucesso!")
+        })
+}
+
+function RedButtonUser({saleId} : Props){
     return(
-       <div className="red-btn">
+       <div className="red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
        </div>
     )
